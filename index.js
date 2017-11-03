db59d32c2b476a5970a84c41e34f65e7a5932580
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2017-11-03 20:03:59 +0100

    update

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const cors = require("cors")({origin: true});
+const c = {
+    "rotate-left": "rotate-left",
+    "rotate-right": "rotate-right",
+    "advance": "advance",
+    "retreat": "retreat",
+    "shoot": "shoot",
+    "pass": "pass"
+}
 
 exports.info = function (req, res) {
     cors(req, res, () => {
-        let content = { name: "Jarle", team: "foo" };
+        let content = { name: "Jarle", team: "Sandvika" };
         res.status(200).send(content);
     })
 };
 
 exports.command = function (req, res) {
     cors(req, res, () => {
-        let content = { command: "shoot"};
+        let content = {
+            command: c.advance
+        };
         res.status(200).send(content);
     })
 };
