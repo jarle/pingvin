4d33a8c9a96c8fa94f4a7f20f0ff48f88c5e04c5
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2017-11-04 01:56:26 +0100

    update

--- index.js
+++ index.js
@@ -172,8 +172,6 @@ exports.command = function (req, res) {
                         return
                     }
                 }
-                res.status(200).send({command: c.advance})
-                return
             }
             // avoid walls
             if(direction === "top") {
@@ -277,6 +275,55 @@ exports.command = function (req, res) {
                 }
             }
             else {
+                // avoid walls (i copy-pasted this because im sleepy)
+                if(direction === "top") {
+                    const block = walls.filter(w => {
+                        const xCollision = ((w.x - x) == 0)
+                        const yCollision = ((y - 1) == w.y)
+                        return (xCollision && yCollision)
+                    })
+                    const collideWithBoundary = (y == 0);
+                    if(block.length > 0 || collideWithBoundary) {
+                        res.status(200).send({command: c.rotateRight})
+                        return
+                    }
+                }
+                else if(direction === "bottom") {
+                    const block = walls.filter(w => {
+                        const xCollision = ((w.x - x) == 0)
+                        const yCollision = ((y + 1) == w.y)
+                        return (xCollision && yCollision)
+                    })
+                    const collideWithBoundary = ((mapHeight - y) == 1)
+                    if(block.length > 0 || collideWithBoundary) {
+                        res.status(200).send({command: c.rotateRight})
+                        return
+                    }
+                }
+                else if(direction === "left") {
+                    const block = walls.filter(w => {
+                        const xCollision = ((x - 1 ) == w.x)
+                        const yCollision = ((w.y - y) == 0)
+                        return (xCollision && yCollision)
+                    })
+                    const collideWithBoundary = (x === 0)
+                    if(block.length > 0 || collideWithBoundary) {
+                        res.status(200).send({command: c.rotateRight})
+                        return
+                    }
+                }
+                else if(direction === "right") {
+                    const block = walls.filter(w => {
+                        const xCollision = ((x + 1 ) == w.x)
+                        const yCollision = ((w.y - y) == 0)
+                        return (xCollision && yCollision)
+                    })
+                    const collideWithBoundary = ((mapWidth - x) == 1)
+                    if(block.length > 0 || collideWithBoundary) {
+                        res.status(200).send({command: c.rotateRight})
+                        return
+                    }
+                }
                 res.status(200).send({command: c.advance})
                 return
             }
